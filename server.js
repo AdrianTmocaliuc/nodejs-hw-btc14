52d444754c9ac40048bfb019a595cc5c7606fcfc
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-06-14 12:28:06 +0300

    adding .env, connect to dbmongoose

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const { DB_HOST, PORT } = process.env;
+
+//hw03-mongodb
+// const DB_HOST =
+//   "mongodb+srv://hw03-mongodb:IPOTbZ76Bqm4IobF@cluster0.ll9dl.mongodb.net/hw03-mongodb?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect success"))
+  .catch((error) => console.log(error.message));
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
