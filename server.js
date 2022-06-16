9bcd063130f1ac6413baca9f35a95db631d66a11
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-06-16 13:31:02 +0300

    change env

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT } = process.env;
-
-//hw03-mongodb
-// const DB_HOST =
-//   "mongodb+srv://hw03-mongodb:IPOTbZ76Bqm4IobF@cluster0.ll9dl.mongodb.net/hw03-mongodb?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log(`Server running on port: ${PORT}`);
+  })
   .then(() => console.log("Database connect success"))
-  .catch((error) => console.log(error.message));
-
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+  .catch((error) => {
+    console.log(error.message)
+    process.exit(1)
+  });
