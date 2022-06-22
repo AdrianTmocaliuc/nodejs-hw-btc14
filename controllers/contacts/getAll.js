36153c841d6b573c19b34978b65abc7cdeac3702
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-06-22 12:55:52 +0300

    adding filter for query param(favorite contacts)

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -5,14 +5,18 @@ const getAll = async (req, res) => {
   const { _id } = req.user;
 
   console.log(req.query);
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner: _id }, "-createdAt -updatedAt", {
-    skip,
-    limit: +limit,
-    // limit: Number(limit),
-  }).populate("owner", "email subscription");
+  const result = await Contact.find(
+    favorite ? { owner: _id, favorite } : { owner: _id },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit: +limit,
+      // limit: Number(limit),
+    }
+  ).populate("owner", "email subscription");
   if (!result) {
     throw generateError(400, "missing field favorite");
   }
