7b2bc91b086f15e2e1f0f14c0ab117962b6737aa
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-06-22 12:58:14 +0300

    small corrections

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -15,7 +15,7 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
   const comparePassword = await bcrypt.compare(password, user.password);
   if (!user || !comparePassword) {
-    throw generateError(401, "Email or passwo   rd is wrong");
+    throw generateError(401, "Email or password is wrong");
   }
 
   const payload = {
@@ -35,19 +35,3 @@ const login = async (req, res) => {
 };
 
 module.exports = login;
-
-// const payload = {
-//   td: "62b09563b1d82f62d14a0e56",
-// };
-
-// const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
-// // console.log("token: ", token);
-// const decodeToken = jwt.decode(token);
-// // console.log(decodeToken);
-
-// try {
-//   const verifyToken = jwt.verify(token, SECRET_KEY);
-//   console.log(verifyToken);
-// } catch (error) {
-//   console.log(error.message);
-// }
