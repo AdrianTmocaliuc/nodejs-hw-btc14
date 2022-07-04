43bb6756c9db46136142bd1838918797cedbd99d
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-07-04 09:07:04 +0300

    correct verification mail

--- controllers/auth/verifyEmail.js
+++ controllers/auth/verifyEmail.js
@@ -2,16 +2,13 @@ const { User } = require("../../models/user");
 
 const { generateError } = require("../../helpers");
 
-const verifyEmail = async (res, req) => {
-  console.log(req.params);
+const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
   const user = await User.findOne({ verificationToken });
 
   if (!user) {
     throw generateError(404);
   }
-
-  // console.log(verificationToken);
 
   await User.findByIdAndUpdate(user._id, {
     verify: true,
